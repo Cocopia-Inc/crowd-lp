bb53f0acbb9969af59c2941cd9a2e9df7f34d772
shuhei909 <shuhei909@gmail.com>
2018-05-23 19:53:01 +0900

    wip

--- js/common.js
+++ js/common.js
@@ -9,35 +9,11 @@ $(function(){
     });
   });
 
-  // kv action
-  setTimeout(function(){
-    $(".top-kv h1").addClass('show')
-  },500);
-  setTimeout(function(){
-    $(".top-kv h1 img").addClass('active')
-  },1500);
-  setTimeout(function(){
-    $(".top-kv h1").addClass('active')
-  },3000);
-  setTimeout(function(){
-    // slick
-    $('.kv-slider').slick({
-      fade: true,
-      arrows: false,
-      autoplay: true,
-      speed: 2000,
-      pauseOnHover: false
-    });
-  },4200);
-  setTimeout(function(){
-    $(".top-kv-img").addClass('active')
-  },4200);
-
 
   // ページ内scroll
   $('a[href^="#"]').click(function() {
     // スクロールの速度
-    var speed = 400; // ミリ秒
+    var speed = 700; // ミリ秒
     // アンカーの値取得
     var href= $(this).attr("href");
     // 移動先を取得
@@ -77,13 +53,5 @@ $(function(){
   // トップ動き wow.js
   new WOW().init();
 
-  // scrollbar
-  $(window).on("load",function(){
-    $(".js-scroll-box").mCustomScrollbar({
-      advanced:{
-        updateOnContentResize: true
-      }
-    });
-  });
-  
+
 });
