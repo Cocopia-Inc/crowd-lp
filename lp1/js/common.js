b7d3a6dc44a0a1b1661ee6add98fb6ed9285aba4
Shohei Ihaya <ihaya.shohei@me.com>
2018-07-18 14:28:42 +0900

    Create 2 versions without formrun

---
+++ lp1/js/common.js
@@ -0,0 +1,43 @@
+$(function(){
+
+  // ページ内scroll
+  $('a[href^="#"]').click(function() {
+    var speed = 700; // ミリ秒
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+
+
+  // $('.js_form_submit').click(function() {
+  //   $('.js_form_content').hide();
+  //   $('.section_contact_complete').show();
+  // });
+
+
+
+});
+
+$(window).on("load resize", function() {
+  var winWidth = $(window).width();
+  if( (winWidth <= 811) ) {
+    // slick slider
+    $('.js-slider').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 2,
+      autoplay: true,
+      arrows: false,
+      responsive: [{
+        breakpoint: 500,settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }],
+    });
+  } else {
+    $('.js-slider').slick('unslick');
+  }
+});
